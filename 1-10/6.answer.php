65dcd98373274926f483c963520509e9a6140d38
Scott <scott@exussum.co.uk>
2013-06-23 01:43:21 +0100

    v Number

---
+++ 1-10/6.answer.php
@@ -0,0 +1,27 @@
+<?php
+/*
+
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+*/
+
+function sumOfSquares ($max) {
+
+return ($max * ($max +1) * (2*$max+1))/6;
+
+}
+
+function squareOfSum($max) {
+$sum = ( $max * ($max+1 )) /2;
+return $sum * $sum;
+}
+
+echo "Answer : " .(  squareOfSum(100) - sumOfSquares(100) ) ;
