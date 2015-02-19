1fdf85f35de14b48cca9d8edc62fa1cdcac60c30
Scott Dutton <scott@exussum.co.uk>
2015-02-19 16:38:02 +0000

    Speed improvements for 3

--- 1-10/3.answer.php
+++ 1-10/3.answer.php
@@ -8,14 +8,17 @@ What is the largest prime factor of the number 600851475143 ?
 $answer = 1;
 $number = 600851475143;
 
-$max= (int)sqrt($number);
-$prime =0;
-while($prime < $max) {
+$prime = 0;
+while($prime < $number) {
 	$prime = (int) gmp_strval(gmp_nextprime($prime));
-	if(fmod($number,$prime) == 0) {//fmod because % doesnt support large numbers
-		$answer =$prime;
-	}	
-
+	while(true){
+		if(fmod($number,$prime) == 0) {
+			$number /= $prime;
+			$answer = $prime;
+		} else { 
+			break;
+		}
+	}
 }
 
 
